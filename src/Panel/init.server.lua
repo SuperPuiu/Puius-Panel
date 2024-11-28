904111000a18250cc0de3473a827b9f2b79bff3a
SuperPuiu <SuperPuiu@proton.me>
2024-11-28 17:18:19 +0200

    Logs
    Added logs on server side. RemoteFunction now can't be called by non-admins fully.

--- src/Panel/init.server.lua
+++ src/Panel/init.server.lua
@@ -4,8 +4,7 @@ local Settings    = require(script.Settings)
 local Permissions = {}
 local PluginsName = {}
 local Plugins     = {}
-
-local InsertService = game:GetService("InsertService")
+local Logs        = {}
 
 local function GivePanel(Player)
   if Player.PlayerGui:FindFirstChild("PanelUI") then return end
@@ -54,11 +53,14 @@ local function Main()
   end
 
   Event.OnServerInvoke = function(Player, Data)
+    if not Permissions.Administrators[Player.UserId] and not Permissions.Moderators[Player.UserId] then return "User is not authorized to call the remote function." end
+
     if Data.Type == "RequestPlugins" then
       return PluginsName
+    elseif Data.Command == "Logs" then
+      return Logs
     else
       local Status, AdditionalResponse = pcall(function()
-        if not Permissions.Administrators[Player.UserId] and not Permissions.Moderators[Player.UserId] then return "User is not authorized to call the remote function." end
         if not Data then return "No data passed." end
 
         local Command = Data.Command
@@ -69,6 +71,15 @@ local function Main()
         if type(Command) ~= "string" or type(Targets) ~= "table" then return "Unknown data types given." end
         return Plugins[Command](Player, Targets, Arguments) or `Executed {Command} command!`
       end)
+
+      if type(Status) == "string" then
+        local NewLog = {}
+        NewLog.ExecutedBy = Player.Name
+        NewLog.Arguments = Arguments
+        NewLog.Command = Command
+
+        table.insert(Logs, NewLog)
+      end
 
       return AdditionalResponse or Status
     end
