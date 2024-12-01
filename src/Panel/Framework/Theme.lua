72ad273ed49583ff2929484f54fab2a76bcfed07
SuperPuiu <SuperPuiu@proton.me>
2024-12-01 21:01:41 +0200

    Preparation for addition of themes.

---
+++ src/Panel/Framework/Theme.lua
@@ -0,0 +1,6 @@
+local Theme = {}
+--[[
+-- This module allows you to overwrite existing panel look. It is recommended to use this module so updating won't break the panel's look
+-- TODO: Implement theme requirements. 
+--]]
+return Theme
